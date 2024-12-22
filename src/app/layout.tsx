20bd1a939d78f037e76d7c13578c9e428cc94c2e
bowadao <samuelibrahim3029@gmail.com>
2024-12-22 19:21:11 +0100

    Updated portfolio SEO metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,38 @@
 import "./globals.css";
 import { ThemeProvider } from "@/providers";
 import { CallToAction, Footer, NavigationBar } from "@/containers";
-import { NeutonRegular, font } from "./font";
+import { font } from "./font";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Samuel Ibrahim Oluwanbowa | Full Stack Developer",
+  description:
+    "Samuel Ibrahim is a full-stack developer and the technical founder of Shopivol, a platform for small business owners to create websites in minutes. Samuel is a skilled developer who builds web application using React, Node.js, Next.js, Express, TypeScript, JavaScript, Tailwind CSS, ShadCN, etc.",
+  keywords: [
+    "Samuel Ibrahim",
+    "Samuel Oluwanbowa Ibrahim",
+    "Full Stack Developer",
+    "Software Developer",
+    "Nigeria-based Software Developer",
+    "Software Engineer",
+    "Talented Software Developer",
+    "Web Developer",
+    "Next.js Developer",
+    "React Developer",
+    "Node.js Developer",
+    "Skilled Developer",
+    "Remote Software Developer",
+    "Website Developer for hire",
+    "Software Developer for hire",
+    "Software Engineer for hire",
+    "Website Development",
+    "Frontend Developer",
+    "Frontend Development",
+    "Full-stack Development",
+    "Full-stack Developer for hire",
+    "Frontend Developer for hire",
+  ],
+  publisher: "Samuel Ibrahim Oluwanbowa",
 };
 
 export default function RootLayout({
